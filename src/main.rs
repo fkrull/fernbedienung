ad1d15b1741b8201f8e848ee716232af4e4e29ef
Felix Krull <f_krull@gmx.de>
2022-02-06 00:14:35 +0100

    Listen for events on input device files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use inotify::{Inotify, WatchMask};
+
+fn main() -> eyre::Result<()> {
+    color_eyre::install()?;
+
+    let mut inotify = Inotify::init()?;
+    inotify.add_watch(
+        "/dev/input",
+        WatchMask::ATTRIB | WatchMask::CREATE | WatchMask::MOVED_TO,
+    )?;
+
+    let mut buffer = [0; 1024];
+    loop {
+        let events = inotify.read_events_blocking(&mut buffer)?;
+        for event in events {
+            println!("event: {:?}", event);
+        }
+    }
 }
